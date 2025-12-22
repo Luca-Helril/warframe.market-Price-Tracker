b064df418d3f5fbb873792576692df1be809ae19
Luca Helril <luca.hiller30916@gmail.com>
2025-12-22 03:12:12 +0100

    cheacking if the item taht is in the pucture is owned

--- PR(Python)/src/png_Text_Extractor.py
+++ PR(Python)/src/png_Text_Extractor.py
@@ -1,21 +1,44 @@
 import easyocr
-
+from Scraping import Scraping
+from WarframeWiki import WarframeWiki
 import sys
+
+import pytesseract
+from PIL import Image
+
 sys.stdout.reconfigure(encoding="utf-8")
 
-reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
+reader = easyocr.Reader(['en'], gpu=True) # this needs to run only once to load the model into memory
 result = reader.readtext('ScreenShot.png')
-primes = []
+words = []
 
 for (bbox, text, prob) in result:
     #print(f'Text: {text}, Probability: {prob}')
     if "Prime" in text:
         print(f'Text: {text}')
-        primes.append(text)
+        split_words = text.split()
+        split_words.append(None)
+        words.append(split_words)
 
-print(primes)
+print(words)
 
 # methode aufrufe die prüft ob die elemente in array bereits in besitz sind
+warframeWiki = WarframeWiki()
+primes_List = warframeWiki.get_primes_list()
+owed_list = warframeWiki.get_owned_list()
+
+
+result = []
+for kategori in primes_List: # katogorien (z.b. Warframes, waffen, ...)
+            for name in primes_List[kategori]: # name der gegensdände (z.b. Garuda, Volt, ...)
+                for item in words: # schauen ob der name des objects mit den namen der von screemshot kommt übereinstimmt
+                    if(name.lower() == item[0].lower()):
+                        item[3] = owed_list[kategori][item[1]][item[2]]
+
+
+print(words)                         
+
+            
 
 
     
--- PR(Python)/src/screen_scan.py
+++ PR(Python)/src/screen_scan.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import mss
 
 
+
 # takes a scranshot of the first monitor
 with mss.mss() as sct:
     filename = sct.shot(output="ScreenShot.png")
